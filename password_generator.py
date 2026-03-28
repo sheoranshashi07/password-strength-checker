7bf79d8c22e5293a674dcd38717bacc42f6f82d9
sheoranshashi07 <shashi.sheoran3@gmail.com>
2026-03-29 00:26:18 +0530

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -1,1 +1,18 @@
+import random
+import string
 
+def generate_password(length):
+
+    characters = string.ascii_letters + string.digits + "@#$%&*"
+
+    password = ""
+
+    for i in range(length):
+        password += random.choice(characters)
+
+    return password
+
+
+length = int(input("Enter password length: "))
+
+print("Generated Password:", generate_password(length))
