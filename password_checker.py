720f2b4bc35c3def0e0411c519d7198cbae5bd7a
sheoranshashi07 <shashi.sheoran3@gmail.com>
2026-03-29 00:21:18 +0530

    Update password_checker.py

--- password_checker.py
+++ password_checker.py
@@ -1,1 +1,35 @@
+import re
+
+def check_password_strength(password):
+    
+    strength = 0
+
+    if len(password) >= 8:
+        strength += 1
+
+    if re.search("[A-Z]", password):
+        strength += 1
+
+    if re.search("[a-z]", password):
+        strength += 1
+
+    if re.search("[0-9]", password):
+        strength += 1
+
+    if re.search("[@#$%^&*!]", password):
+        strength += 1
+
+    if strength <= 2:
+        return "Weak Password"
+    elif strength == 3 or strength == 4:
+        return "Moderate Password"
+    else:
+        return "Strong Password"
+
+
+password = input("Enter your password: ")
+
+result = check_password_strength(password)
+
+print("Password Strength:", result)
 
