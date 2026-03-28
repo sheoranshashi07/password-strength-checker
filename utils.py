411d7a8cf5c5a62401060633d6824e2a25190c3d
sheoranshashi07 <shashi.sheoran3@gmail.com>
2026-03-29 00:35:34 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,1 +1,20 @@
+import re
 
+# Check if password contains uppercase letter
+def has_uppercase(password):
+    return bool(re.search("[A-Z]", password))
+
+
+# Check if password contains lowercase letter
+def has_lowercase(password):
+    return bool(re.search("[a-z]", password))
+
+
+# Check if password contains a number
+def has_number(password):
+    return bool(re.search("[0-9]", password))
+
+
+# Check if password contains special characters
+def has_special_character(password):
+    return bool(re.search("[@#$%^&*!]", password))
